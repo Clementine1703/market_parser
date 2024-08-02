c5ce1a282daef3df6a67d2cc1fad31e82882fbdd
Камиль Алиметов <kamil.alimetov@gmail.com>
2024-08-02 19:49:56 +0300

    -

--- importer.py
+++ importer.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from django.core.files import File
 from slugify import slugify
 
-from src.config import BASE_DIR
+from parser.src.config import BASE_DIR
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
@@ -53,7 +53,7 @@ with open(f'{BASE_DIR}/data.json', 'r') as f:
         weigth_list = [0]
         if data.get('weigth', False):
             weigth_list = []
-            weigth_list.append(data['weigth'].split(' ')[0])
+            weigth_list.append(data['weigth'].split(' ')[1])
             print(weigth_list, 1)
         elif data.get('options_for_selection'):
             for el in data.get('options_for_selection'):
@@ -64,56 +64,69 @@ with open(f'{BASE_DIR}/data.json', 'r') as f:
 
 
         # for weigth in weigth_list:
-            slug = slugify(f"{data['title']}-asasdfwefgbrtrrfdasdf")
+        slug = slugify(f"{data['title']}-asacsdfwefgbrtsdfrasdfassffffdrdfgfasdfdasdf")
 
-            # Создаем или обновляем продукт
-            product, created = Product.objects.get_or_create(title=data['title'], sale=0, slug=slug)
-
-            # Обновляем поля продукта
-            product.description = data['application']
-            # product.weight = weigth
+        category_slug = data['category_slug']
+        if category_slug in category_matching_table:
+            category = Category.objects.get(slug=category_matching_table[category_slug])
+            
+            product, created = Product.objects.get_or_create(title=data['title'], sale=0, slug=slug, isPublic=True)
             product.save()
 
-            category = Category.objects.filter(slug=category_matching_table[data['category_slug']]).first()
-            product.category.add(category)
+            # Убедитесь, что поле category инициализировано
+            if product.category is None:
+                product.category = category
+                product.save()
+        else:
+            continue
+        
+        # Обновляем поля продукта
+        product.description = data['application']
+        product.weight = weigth_list[0]
+        product.save()
 
-            # Обрабатываем изображения
-            for image_path in data['images']:
-                with open(f'{BASE_DIR}/{image_path}', 'rb') as f:
-                    django_file = File(f)
-                    product.image.save(os.path.basename(image_path), django_file, save=True)
+        category = Category.objects.filter(slug=category_matching_table[data['category_slug']]).first()
 
-            for cert in data['certs']:
-                with open(f"{BASE_DIR}/{cert['path']}", 'rb') as f:
-                    django_file = File(f)
-                    slug = slugify(f"{cert['name']}-asdadfgbtwertsasdrffdf")
-                    if Documentation.objects.filter(slug=slug).exists():
-                        raise ValueError(f"Slug '{slug}' already exists.")
-                    doc, created = Documentation.objects.get_or_create(title=cert['name'], slug=slug)
-                    doc.file_download.save(os.path.basename(cert['path']), django_file, save=True)
-                    product.documents.add(doc)
+        # Обрабатываем изображения
+        for image_path in data['images']:
+            with open(f'{BASE_DIR}/{image_path}', 'rb') as f:
+                django_file = File(f)
+                product.image.save(os.path.basename(image_path), django_file, save=True)
 
-            for feature in data['featues']:
-                if feature:
+        for id, cert in enumerate(data['certs']):
+            with open(f"{BASE_DIR}/{cert['path']}", 'rb') as f:
+                django_file = File(f)
+                slug = slugify(f"product-{product.id}-{cert['name']}-fdasdfsdfasvv-{id}")
+                if Documentation.objects.filter(slug=slug).exists():
+                    raise ValueError(f"Slug '{slug}' already exists.")
+                doc, created = Documentation.objects.get_or_create(title=cert['name'], slug=slug)
+                doc.file_download.save(os.path.basename(cert['path']), django_file, save=True)
+                product.documents.add(doc)
+
+        for feature in data['featues']:
+            if feature:
+                try:
                     key, value = feature[0], feature[1]
-                    prop, _ = MainCharacteristic.objects.get_or_create(title=key)
-                    characteristic, created = Characteristic.objects.get_or_create(product=product, prop=prop, val=value)
-                    characteristic.save()
-            
+                except:
+                    continue
+                prop, _ = MainCharacteristic.objects.get_or_create(title=key)
+                characteristic, created = Characteristic.objects.get_or_create(product=product, prop=prop, val=value)
+                characteristic.save()
+        
 
 
 
-            # Обрабатываем преимущества
-            # for advantage in data['advantages']:
-                # ...
-                # Здесь можно создать модель для преимуществ и связать её с продуктом
-                # Например: Advantage.objects.get_or_create(product=product, text=advantage)
+        # Обрабатываем преимущества
+        # for advantage in data['advantages']:
+            # ...
+            # Здесь можно создать модель для преимуществ и связать её с продуктом
+            # Например: Advantage.objects.get_or_create(product=product, text=advantage)
 
-            # Обрабатываем опции выбора
-            # for option in data['options_for_selection']:
-            #     title = option['title']
-            #     values = option['values']
-                # Здесь можно создать модель для опций выбора и связать её с продуктом
-                # Например: Option.objects.get_or_create(product=product, title=title, values=values)
+        # Обрабатываем опции выбора
+        # for option in data['options_for_selection']:
+        #     title = option['title']
+        #     values = option['values']
+            # Здесь можно создать модель для опций выбора и связать её с продуктом
+            # Например: Option.objects.get_or_create(product=product, title=title, values=values)
 
-            print(f"Product {product.title} has been {'created' if created else 'updated'}.")
+        print(f"Product {product.title} has been {'created' if created else 'updated'}.")
